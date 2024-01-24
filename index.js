f30b0a5e1167e57249dc60a0ec42cb76a7a053e7
harsh <harshdeep1043@gmail.com>
2024-01-24 21:46:02 +0530

    4th commit

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/holinfo",holinfoRouter);
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log("backend is running");
 });
